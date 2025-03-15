1b0147a24637d12980f986f4b581ca6542bb9928
Boicu David <david.boicu@student.usv.ro>
2025-03-15 17:48:54 +0200

    up

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-import { JwtPayload } from '../interfaces/jwt-payload.interface'; // Ensure this is defined
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extracts JWT from Authorization header
-      secretOrKey: process.env.JWT_SECRET, // Secret key from environment variables
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET, 
     });
   }
 
   async validate(payload: JwtPayload) {
-    // Check if user exists, or add any other validation logic
-    console.log('Decoded JWT Payload:', payload);
     return { userId: payload.sub, email: payload.email, roles: payload.roles };
   }
 }
