a807219703c1b221e57eae9b3f4cc57218630983
NistorElena-Simona <elena.nistor1@student.usv.ro>
2025-03-21 20:26:00 +0200

    initial

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', 
+    credentials: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
